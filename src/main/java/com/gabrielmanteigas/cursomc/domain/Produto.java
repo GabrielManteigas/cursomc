907a35a51148edbd5028e6b6cc1c03d511ec5f48
Gabriel Manteigas <40912@alunos.isel.ipl.pt>
2021-02-25 21:51:18 +0000

    Criando repository e service para Categoria

---
+++ src/main/java/com/gabrielmanteigas/cursomc/domain/Produto.java
@@ -0,0 +1,11 @@
+package com.gabrielmanteigas.cursomc.domain;
+
+
+public class Produto {
+	
+	private Integer id;
+	private String nome;
+	
+	public Produto() {}
+	
+}
---
+++ src/main/java/com/gabrielmanteigas/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.gabrielmanteigas.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielmanteigas.cursomc.domain.Categoria;
+import com.gabrielmanteigas.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
