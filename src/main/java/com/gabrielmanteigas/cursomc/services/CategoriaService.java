98d9cd6e78d4a45f315874eefbee2b98f38340cb
Gabriel Manteigas <40912@alunos.isel.ipl.pt>
2021-03-03 03:47:42 +0000

    Ajustes no endpoint /categoria{id}

--- src/main/java/com/gabrielmanteigas/cursomc/services/CategoriaService.java
+++ src/main/java/com/gabrielmanteigas/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.gabrielmanteigas.cursomc.domain.Categoria;
 import com.gabrielmanteigas.cursomc.repositories.CategoriaRepository;
+import com.gabrielmanteigas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+					"Objecto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
