29213038c6de266269528cf8468cd274c85d617d
Gabriel Manteigas <40912@alunos.isel.ipl.pt>
2021-03-14 14:34:57 +0000

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/gabrielmanteigas/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,12 @@
+package com.gabrielmanteigas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabrielmanteigas.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+	
+
+}
