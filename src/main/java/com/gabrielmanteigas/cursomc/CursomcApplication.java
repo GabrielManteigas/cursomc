5adcaa575b8ca489bd5c3887c59f5c2652421d6c
Gabriel Manteigas <40912@alunos.isel.ipl.pt>
2021-03-14 11:30:42 +0000

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/gabrielmanteigas/cursomc/CursomcApplication.java
+++ src/main/java/com/gabrielmanteigas/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.gabrielmanteigas.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.gabrielmanteigas.cursomc.domain.Cidade;
 import com.gabrielmanteigas.cursomc.domain.Cliente;
 import com.gabrielmanteigas.cursomc.domain.Endereco;
 import com.gabrielmanteigas.cursomc.domain.Estado;
+import com.gabrielmanteigas.cursomc.domain.Pagamento;
+import com.gabrielmanteigas.cursomc.domain.PagamentoComBoleto;
+import com.gabrielmanteigas.cursomc.domain.PagamentoComCartao;
+import com.gabrielmanteigas.cursomc.domain.Pedido;
 import com.gabrielmanteigas.cursomc.domain.Produto;
+import com.gabrielmanteigas.cursomc.domain.enums.EstadoPagamento;
 import com.gabrielmanteigas.cursomc.domain.enums.TipoCliente;
 import com.gabrielmanteigas.cursomc.repositories.CategoriaRepository;
 import com.gabrielmanteigas.cursomc.repositories.CidadeRepository;
 import com.gabrielmanteigas.cursomc.repositories.ClienteRepository;
 import com.gabrielmanteigas.cursomc.repositories.EnderecoRepository;
 import com.gabrielmanteigas.cursomc.repositories.EstadoRepository;
+import com.gabrielmanteigas.cursomc.repositories.PagamentoRepository;
+import com.gabrielmanteigas.cursomc.repositories.PedidoRepository;
 import com.gabrielmanteigas.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -90,6 +104,26 @@ public class CursomcApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"),cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"),cli1, e2);
+		
+		Pagamento pagto1= new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2= new PagamentoComBoleto(
+				null, 
+				EstadoPagamento.PENDENTE, 
+				ped2, 
+				sdf.parse("20/10/2017 00:00"), 
+				null
+				);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 	}
 
 }
---
+++ src/main/java/com/gabrielmanteigas/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,12 @@
+package com.gabrielmanteigas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabrielmanteigas.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+	
+
+}
